9b2e2b8af878834f6ae16f1b0e3893621d96adb0
Anthony Spencer <33986201+SPEN84@users.noreply.github.com>
2019-04-18 01:57:12 -0400

    Add files via upload

---
+++ thereyet.js
@@ -0,0 +1,10 @@
+var answer = prompt(" are we there yet?");
+
+while(answer.indexOf("yes")) {
+	var answer = prompt("are we there yet?");
+}
+
+alert("YAY, WE MADE IT !!!");
+
+/* this code is an example of a loop searching for the phrase and 
+ not the exact word for the answer by using indexOF method */
